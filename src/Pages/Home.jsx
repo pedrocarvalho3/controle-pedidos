9b61093d36c61964d733665e0f319602306a185b
pedrocarvalho3 <pdrpvp0310@gmail.com>
2023-04-20 18:01:08 -0300

    Applying responsivity

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -34,22 +34,22 @@ export default function Home() {
   return (
     <div className="container mx-auto w-full flex flex-col items-center py-8 gap-8">
       <h1 className="text-4xl font-bold text-gray-700">Pedidos</h1>
-      <div className="text-lg w-full">
-        <div className="flex justify-between md:grid grid-cols-4 text-gray-200 bg-teal-600 rounded-t-lg py-4 px-8">
+      <div className="lg:max-w-6xl text-md sm:text-lg w-full">
+        <div className="flex justify-between sm:grid grid-cols-[100px_1fr_1fr_1fr] text-gray-200 bg-teal-600 rounded-t-lg py-2 px-4 md:py-4 md:px-8">
           <h3>ID</h3>
           <h3>Cliente</h3>
           <h3>Data</h3>
           <span></span>
         </div>
         {data.map((pedido) => (
-          <div className="flex justify-between md:grid grid-cols-4 text-gray-700 bg-gray-200 last:rounded-b-lg  py-4 px-8">
+          <div className="flex justify-between sm:grid grid-cols-[100px_1fr_1fr_1fr] text-gray-700 bg-gray-200 last:rounded-b-lg py-2 px-4 md:py-4 md:px-8">
             <p>{pedido.id}</p>
             <p>{pedido.clienteNome}</p>
             <p>{pedido.data}</p>
             <a href={`/informacoes-pedido/${pedido.id}`}>
               <button
                 type="submit"
-                className="text-gray-200 bg-teal-600 px-4 py-2 rounded-md hover:bg-teal-500"
+                className="text-gray-200 bg-teal-600 px-4 py-2 rounded-md hover:bg-teal-500 ml-auto transition"
               >
                 Ver Informações
               </button>
--- src/Pages/InformacoesPedido.jsx
+++ src/Pages/InformacoesPedido.jsx
@@ -47,38 +47,40 @@ export default function InformacoesPedido() {
   }, [id, produtos]);
 
   return (
-    <div className="container mx-auto w-full flex flex-col items-center py-8 gap-8 text-lg text-gray-700">
+    <div className="container mx-auto w-full flex flex-col items-center py-8 gap-8 text-gray-700 px-4">
       <h1 className="text-4xl font-bold ">Informações do Pedido</h1>
 
-      <div className="text-lg text-center">
+      <div className="lg:max-w-6xl text-md sm:text-lg text-center">
         <div>
-          <div className="flex justify-between md:grid grid-cols-4 text-gray-200 bg-teal-600 rounded-t-lg py-4 px-8">
+          <div className="flex justify-between md:grid grid-cols-[100px_1fr_1fr_1fr] text-gray-200 bg-teal-600 rounded-t-lg py-2 px-4 md:py-4 md:px-8">
             <h3>Data</h3>
             <h3>Nome</h3>
             <h3>Email</h3>
             <h3>CPF</h3>
           </div>
-          <div className="flex justify-between md:grid grid-cols-4 text-gray-700 bg-gray-200 last:rounded-b-lg  py-4 px-8">
+          <div className="flex justify-between items-center sm:grid grid-cols-[100px_1fr_1fr_1fr] text-gray-700 bg-gray-200 last:rounded-b-lg  py-2 px-4 md:py-4 md:px-8">
             <p>{pedido.data}</p>
             <p>{cliente.nome}</p>
             <p>{cliente.email}</p>
             <p>{cliente.cpf}</p>
           </div>
         </div>
-        <div className="flex justify-between md:grid grid-cols-2 text-gray-200 bg-teal-600 rounded-t-lg py-4 px-8 mt-8">
+        <div className="flex justify-between sm:grid grid-cols-[400px_1fr] text-gray-200 bg-teal-600 rounded-t-lg py-2 px-4 md:py-4 md:px-8 mt-8">
           <h3>Produto Nome</h3>
           <h3>Quantidade</h3>
         </div>
         {itemsPedido.map((item) => (
-          <div className="flex justify-between md:grid grid-cols-2 bg-gray-200 last:rounded-b-md py-4 px-8">
+          <div className="flex justify-between items-center sm:grid grid-cols-[400px_1fr] bg-gray-200 last:rounded-b-md py-2 px-4 md:py-4 md:px-8">
             <p>{item.produtoNome}</p>
             <p>{item.quantidade}</p>
           </div>
         ))}
       </div>
-      <button className="text-gray-200 bg-teal-600 px-4 py-2 rounded-md hover:bg-teal-500">
-        <a href="/">Voltar a Página de Pedidos</a>
-      </button>
+      <a href="/">
+        <button className="text-gray-200 bg-teal-600 px-4 py-2 rounded-md hover:bg-teal-500 transition">
+          Voltar a Página de Pedidos
+        </button>
+      </a>
     </div>
   );
 }
